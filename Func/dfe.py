cf0dadb72afed06e49a02884164a6a17ee5f296d
aLosada7 <aldc30sc@gmail.com>
2017-01-13 14:20:43 +0100

    Muxa mierda

---
+++ Func/dfe.py
@@ -0,0 +1,8 @@
+import Suma.py
+print("Introducir el valor para A:")
+A = input()
+print("Introducir el valor para B:")
+B = input()
+C = suma(int(A),int(B))
+print("El resultado es: ", C ,".")
+H= input()
